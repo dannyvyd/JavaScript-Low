d77932790698a3f1ae2661fd3c5e55665fdc5348
Daniel Lopez <70736424+dannyvyd@users.noreply.github.com>
2025-03-24 20:30:20 -0500

    Iniciando
    Variables y tipos de datos

---
+++ js/noti.js
@@ -0,0 +1,20 @@
+var nombre = "Daniel";
+var apellido ="Lopez";
+var edad = 23;
+var PresenteOno = true;
+var estatura = 1.67;
+
+// Valores en consola solamente
+
+console.log("Nombre: " + nombre + " " + typeof nombre);
+console.log("Apellido: " + apellido + " " + typeof apellido);
+console.log("Edad: " + edad + " " + typeof edad);
+console.log("PresenteOno: " + PresenteOno + " " + typeof PresenteOno);
+console.log("Estatura: " + estatura + " " + typeof estatura);
+
+alert("Hola mundo con JS, Soy " + nombre + " " + apellido + " y tengo " + edad);
+
+// let es declarado para un solo bloque de codigo en especifico
+
+// const establece un dato o variable para un solo uso durante todo el programa
+
